15b341f1d572fc55f3e43f7c282ec14c33d06f33
Gustavo Marques Reis <gustavo_marquesr@discente.ufg.br>
2022-12-03 11:13:30 -0300

    Update producer.py

--- producer.py
+++ producer.py
@@ -2,6 +2,7 @@ from kafka import KafkaProducer
 from const import *
 from random import randint
 from threading import Thread
+import sys
 import time
 
 #Funcao para enviar topicos
@@ -15,11 +16,24 @@ def send_topic(topic, producer):
 
     producer.flush()
 
+#Argumento que define quantos topicos este produtor vai produzir
+try:
+  number_topics = int(sys.argv[1])
+except:
+  print ('Usage: python3 producer <number_of_topics>')
+  exit(1)
+
 producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
 
-#Loop pra ficar criando novos topicos
-while (True):
-    topic = input('Enter a new topic')
+#Array de topicos
+topics = []
+#Loop para pegar os topicos
+for x in range(number_topics):
+    topic = input('Enter a new topic:\n')
+    topics.append(topic)
+
+#Loop pra enviar todos os topicos
+for topic in topics:
     #Nova thread e criada para enviar cada topico
     thread = Thread(target=send_topic, args=(topic, producer))
     thread.start()
